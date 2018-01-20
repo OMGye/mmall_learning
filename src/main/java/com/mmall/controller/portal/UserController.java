8ecd887141976653045e7dd4fe444d6b4662a4c3
OMGye <1252078414@qq.com>
2018-01-20 18:12:36 +0800

    user

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,187 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by upupgogogo on 2018/1/17.下午8:16
+ */
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+    /**
+     *1.用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username,password);
+        if (response.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    /**
+     * 2.用户退出
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    /**
+     *3.用户注册
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "register.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> register(User user) {
+
+        return iUserService.register(user);
+    }
+
+    /**
+     * 4.校验是否有效
+     * @param str
+     * @param type
+     * @return
+     */
+    @RequestMapping(value = "check_valid.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str, String type){
+
+        return iUserService.checkValid(str,type);
+    }
+
+    /**
+     * 5.获得用户信息
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user != null){
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获得当前用户信息");
+    }
+
+    /**
+     * 6.找回密码
+     * @param username
+     * @return
+     */
+    @RequestMapping(value = "forget_get_question.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetQuestion(String username){
+        return iUserService.selectQuestion(username);
+    }
+
+    /**
+     * 7.回答问题答案
+     * @param username
+     * @param question
+     * @param answer
+     * @return
+     */
+    @RequestMapping(value = "forget_check_answer.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username, String question, String answer){
+        return iUserService.checkAnswer(username,question,answer);
+    }
+
+    /**
+     * 8.重置密码
+     * @param username
+     * @param passwordNew
+     * @param forgetToken
+     * @return
+     */
+    @RequestMapping(value = "forget_reset_password.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetRestPassword(String username, String passwordNew, String forgetToken){
+        return iUserService.forgetRestPassword(username,passwordNew,forgetToken);
+    }
+
+    /**
+     * 9.登录状态下的重置密码
+     * @param session
+     * @param passwordOld
+     * @param passwordNew
+     * @return
+     */
+    @RequestMapping(value = "reset_password.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> resetPassword(HttpSession session, String passwordOld, String passwordNew){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        return iUserService.resetPassword(passwordOld,passwordNew,user);
+    }
+
+    /**
+     * 10.更新个人信息
+     * @param session
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "update_infomation.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> updateInfomation(HttpSession session, User user){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if (currentUser == null) {
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        user.setId(currentUser.getId());
+        user.setUsername(currentUser.getUsername());
+        ServerResponse<User> response = iUserService.updateInfomation(user);
+        if (response.isSuccess()){
+            response.getData().setUsername(currentUser.getUsername());
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    /**
+     * 获得个人信息
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "get_infomation.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getInfomation(HttpSession session){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if (currentUser == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登录，需要强制登录status=10");
+        }
+        return iUserService.getInfomation(currentUser.getId());
+    }
+
+}
---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,20 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+/**
+ * Created by upupgogogo on 2018/1/18.下午6:59
+ */
+public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+    ServerResponse<String> register(User user);
+    ServerResponse<String> checkValid(String str, String type);
+    ServerResponse selectQuestion(String username);
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+    ServerResponse<String> forgetRestPassword(String username, String passwordNew, String forgetToken);
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+    ServerResponse<User> updateInfomation(User user);
+    ServerResponse<User> getInfomation(Integer userId);
+  }
